3915e0451aa8339f89c3d6c5d7a6f1432bea0a98
kumarshantanu <kumarshan25@gmail.comDD>
2026-03-12 14:50:24 +0530

    Revenue Recognition Fixes

--- mcq/quiz/tests.py
+++ mcq/quiz/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from quiz.views import _render_stem_html
+
+
+class StemRenderingTests(TestCase):
+    def test_br_tags_become_rendered_line_breaks(self):
+        html = str(_render_stem_html("Line 1<br>Line 2"))
+        self.assertIn("Line 1<br>Line 2", html)
+
+    def test_other_html_is_escaped(self):
+        html = str(_render_stem_html("Safe<script>alert(1)</script>"))
+        self.assertIn("&lt;script&gt;alert(1)&lt;/script&gt;", html)
